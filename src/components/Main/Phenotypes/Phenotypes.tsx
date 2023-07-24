83456b0c00730570d86a22bc4b694c362ccd204c
Gal Trapečar <trapecar.gal@gmail.com>
2023-07-24 23:28:07 +0200

    implemented selection

--- src/components/Main/Phenotypes/Phenotypes.tsx
+++ src/components/Main/Phenotypes/Phenotypes.tsx
@@ -1,27 +1,33 @@
 import React, { useEffect, useState } from "react"
 import "./Phenotypes.css"
 
-
+function Phenotype({ image, id, selected, setSelectedPhenotype }: { image: string, id: string, selected: boolean, setSelectedPhenotype: (string: string) => void }) {
+    return (
+        <div className={`Phenotypes__image ${selected ? "selected" : ""}`} onClick={() => {
+            setSelectedPhenotype(id)
+        }}>
+            <img src={image} />
+        </div>
+    )
+}
 
 export function Phenotypes({ images }: { images: string[][] }) {
-    const [selectedPhenotype, setSelectedPhenotype] = useState("")
-    
-    function Phenotype({ image, id }: { image: string, id: string }) {
-        const [selected, setSelected] = useState(false)
-        return (
-            <div className={`Phenotypes__image ${selected ? "selected" : ""}`} onClick={() => {
-                setSelected(!selected)
-                setSelectedPhenotype(id)
-            }}>
-                <img src={image} />
-            </div>
-        )
+    const [selectedPhenotypes, setSelectedPhenotypes] = useState<string[]>([])
+
+    const setSelectedPhenotypesCallback = (string: string) => {
+        if (selectedPhenotypes.length == 2) {
+            let old = selectedPhenotypes.pop();
+            selectedPhenotypes.pop();
+            setSelectedPhenotypes([old as string, string])
+        } else {
+            setSelectedPhenotypes([...selectedPhenotypes, string])
+        }
     }
 
     return (
         <div className="Phenotypes">
             {images.map(image => (
-                <Phenotype image={image[0]} id={image[1]} />
+                <Phenotype image={image[0]} id={image[1]} selected={selectedPhenotypes.includes(image[1])} key={image[1]} setSelectedPhenotype={setSelectedPhenotypesCallback} />
             ))}
         </div>
     )
